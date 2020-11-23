917d94592427ae9c18e19b2d24d0ef28be1f9310
Artik <mail@artik.me>
2020-11-23 23:38:20 +0300

    ADD: night theme; additional core color vars

--- docs/script.js
+++ docs/script.js
@@ -33,10 +33,10 @@ const downloadTheme = (form) => {
     /* 
       Change this parameters to update your theme 
     */
-    --${theme}-h: ${styles.getPropertyValue('--my-theme-h')};
-    --${theme}-s: ${styles.getPropertyValue('--my-theme-s')};
-    --${theme}-l: ${styles.getPropertyValue('--my-theme-l')};
-    --${theme}-contrast-threshold: ${styles.getPropertyValue('--my-theme-contrast-threshold')};
+    --${theme}-h: ${styles.getPropertyValue('--${theme}-h')};
+    --${theme}-s: ${styles.getPropertyValue('--${theme}-s')};
+    --${theme}-l: ${styles.getPropertyValue('--${theme}-l')};
+    --${theme}-contrast-threshold: ${styles.getPropertyValue('--${theme}-contrast-threshold')};
     
     
     /* 
@@ -44,37 +44,94 @@ const downloadTheme = (form) => {
       Example:
           background: var(--${theme}-500);
           color: var(--${theme}-500-contrast);
+          
+      To change opacity of your color, use ...-h, ...-s and ...-l variables.
+      Example:
+          background-color: hsla( var(--${theme}-300-h), var(--${theme}-300-s), var(--${theme}-300-l), 70%);
     */
-    --${theme}: hsl(var(--${theme}-h), calc(var(--${theme}-s) * 1%), calc(var(--${theme}-l) * 1%));
-    --${theme}-50: hsl( var(--${theme}-h), calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-50) + var(--mtc-light-s)) * 1%), calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-50) + var(--mtc-light-l)) * 1%) );
-    --${theme}-50-contrast: hsl( 0, 0%, calc(((((1 - var(--mtc-l-50)) * 100 + var(--mtc-l-50) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)) );
-    --${theme}-100: hsl( var(--${theme}-h), calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-100) + var(--mtc-light-s)) * 1%), calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-100) + var(--mtc-light-l)) * 1%) );
-    --${theme}-100-contrast: hsl( 0, 0%, calc(((((1 - var(--mtc-l-100)) * 100 + var(--mtc-l-100) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)) );
-    --${theme}-200: hsl( var(--${theme}-h), calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-200) + var(--mtc-light-s)) * 1%), calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-200) + var(--mtc-light-l)) * 1%) );
-    --${theme}-200-contrast: hsl( 0, 0%, calc(((((1 - var(--mtc-l-200)) * 100 + var(--mtc-l-200) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)) );
-    --${theme}-300: hsl( var(--${theme}-h), calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-300) + var(--mtc-light-s)) * 1%), calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-300) + var(--mtc-light-l)) * 1%) );
-    --${theme}-300-contrast: hsl( 0, 0%, calc(((((1 - var(--mtc-l-300)) * 100 + var(--mtc-l-300) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)) );
-    --${theme}-400: hsl( var(--${theme}-h), calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-400) + var(--mtc-light-s)) * 1%), calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-400) + var(--mtc-light-l)) * 1%) );
-    --${theme}-400-contrast: hsl( 0, 0%, calc(((((1 - var(--mtc-l-400)) * 100 + var(--mtc-l-400) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)) );
+    --${theme}: hsla(var(--${theme}-h), calc(var(--${theme}-s) * 1%), calc(var(--${theme}-l) * 1%), 100%);
+
+    --${theme}-50-h: var(--${theme}-h);
+    --${theme}-50-s: calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-50) + var(--mtc-light-s)) * 1%);
+    --${theme}-50-l: calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-50) + var(--mtc-light-l)) * 1%);
+    --${theme}-50: hsl(var(--${theme}-50-h), var(--${theme}-50-s), var(--${theme}-50-l));
+    --${theme}-50-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-50)) * 100 + var(--mtc-l-50) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-100-h: var(--${theme}-h);
+    --${theme}-100-s: calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-100) + var(--mtc-light-s)) * 1%);
+    --${theme}-100-l: calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-100) + var(--mtc-light-l)) * 1%);
+    --${theme}-100: hsl(var(--${theme}-100-h), var(--${theme}-100-s), var(--${theme}-100-l));
+    --${theme}-100-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-100)) * 100 + var(--mtc-l-100) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-200-h: var(--${theme}-h);
+    --${theme}-200-s: calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-200) + var(--mtc-light-s)) * 1%);
+    --${theme}-200-l: calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-200) + var(--mtc-light-l)) * 1%);
+    --${theme}-200: hsl(var(--${theme}-200-h), var(--${theme}-200-s), var(--${theme}-200-l));
+    --${theme}-200-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-200)) * 100 + var(--mtc-l-200) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-300-h: var(--${theme}-h);
+    --${theme}-300-s: calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-300) + var(--mtc-light-s)) * 1%);
+    --${theme}-300-l: calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-300) + var(--mtc-light-l)) * 1%);
+    --${theme}-300: hsl(var(--${theme}-300-h), var(--${theme}-300-s), var(--${theme}-300-l));
+    --${theme}-300-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-300)) * 100 + var(--mtc-l-300) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-400-h: var(--${theme}-h);
+    --${theme}-400-s: calc(((var(--${theme}-s) - var(--mtc-light-s)) * var(--mtc-s-400) + var(--mtc-light-s)) * 1%);
+    --${theme}-400-l: calc(((var(--${theme}-l) - var(--mtc-light-l)) * var(--mtc-l-400) + var(--mtc-light-l)) * 1%);
+    --${theme}-400: hsl(var(--${theme}-400-h), var(--${theme}-400-s), var(--${theme}-400-l));
+    --${theme}-400-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-400)) * 100 + var(--mtc-l-400) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
     --${theme}-500: var(--${theme});
     --${theme}-500-contrast: hsl(0, 0%, calc(((var(--${theme}-l) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
-    --${theme}-600: hsl( var(--${theme}-h), calc(((1 - var(--mtc-s-600)) * 100 + var(--mtc-s-600) * var(--${theme}-s)) * 1%), calc(((1 - var(--mtc-l-600)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-600) * var(--${theme}-l)) * 1%) );
-    --${theme}-600-contrast: hsl( 0, 0%, calc( ((((1 - var(--mtc-l-600)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-600) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100) ) );
-    --${theme}-700: hsl( var(--${theme}-h), calc(((1 - var(--mtc-s-700)) * 100 + var(--mtc-s-700) * var(--${theme}-s)) * 1%), calc(((1 - var(--mtc-l-700)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-700) * var(--${theme}-l)) * 1%) );
-    --${theme}-700-contrast: hsl( 0, 0%, calc( ((((1 - var(--mtc-l-700)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-700) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100) ) );
-    --${theme}-800: hsl( var(--${theme}-h), calc(((1 - var(--mtc-s-800)) * 100 + var(--mtc-s-800) * var(--${theme}-s)) * 1%), calc(((1 - var(--mtc-l-800)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-800) * var(--${theme}-l)) * 1%) );
-    --${theme}-800-contrast: hsl( 0, 0%, calc( ((((1 - var(--mtc-l-800)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-800) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100) ) );
-    --${theme}-900: hsl( var(--${theme}-h), calc(((1 - var(--mtc-s-900)) * 100 + var(--mtc-s-900) * var(--${theme}-s)) * 1%), calc(((1 - var(--mtc-l-900)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-900) * var(--${theme}-l)) * 1%) );
-    --${theme}-900-contrast: hsl( 0, 0%, calc( ((((1 - var(--mtc-l-900)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-900) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100) ) );
-    --${theme}-A100: hsl(calc(var(--${theme}-h) * var(--mtc-h-A100)), calc(var(--mtc-s-A100) * 100%), calc(var(--mtc-l-A100) * 100%));
+
+    --${theme}-600-h: var(--${theme}-h);
+    --${theme}-600-s: calc(((1 - var(--mtc-s-600)) * 100 + var(--mtc-s-600) * var(--${theme}-s)) * 1%);
+    --${theme}-600-l: calc(((1 - var(--mtc-l-600)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-600) * var(--${theme}-l)) * 1%);
+    --${theme}-600: hsl(var(--${theme}-600-h), var(--${theme}-600-s), var(--${theme}-600-l));
+    --${theme}-600-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-600)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-600) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-700-h: var(--${theme}-h);
+    --${theme}-700-s: calc(((1 - var(--mtc-s-700)) * 100 + var(--mtc-s-700) * var(--${theme}-s)) * 1%);
+    --${theme}-700-l: calc(((1 - var(--mtc-l-700)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-700) * var(--${theme}-l)) * 1%);
+    --${theme}-700: hsl(var(--${theme}-700-h), var(--${theme}-700-s), var(--${theme}-700-l));
+    --${theme}-700-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-700)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-700) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-800-h: var(--${theme}-h);
+    --${theme}-800-s: calc(((1 - var(--mtc-s-800)) * 100 + var(--mtc-s-800) * var(--${theme}-s)) * 1%);
+    --${theme}-800-l: calc(((1 - var(--mtc-l-800)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-800) * var(--${theme}-l)) * 1%);
+    --${theme}-800: hsl(var(--${theme}-800-h), var(--${theme}-800-s), var(--${theme}-800-l));
+    --${theme}-800-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-800)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-800) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-900-h: var(--${theme}-h);
+    --${theme}-900-s: calc(((1 - var(--mtc-s-900)) * 100 + var(--mtc-s-900) * var(--${theme}-s)) * 1%);
+    --${theme}-900-l: calc(((1 - var(--mtc-l-900)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-900) * var(--${theme}-l)) * 1%);
+    --${theme}-900: hsl(var(--${theme}-900-h), var(--${theme}-900-s), var(--${theme}-900-l));
+    --${theme}-900-contrast: hsl(0, 0%, calc(((((1 - var(--mtc-l-900)) * var(--${theme}-l) * var(--${theme}-l) / 100 + var(--mtc-l-900) * var(--${theme}-l)) * 1%) - var(--${theme}-contrast-threshold, 50%)) * (-100)));
+
+    --${theme}-A100-h: calc(var(--${theme}-h) * var(--mtc-h-A100));
+    --${theme}-A100-s: calc(var(--mtc-s-A100) * 100%);
+    --${theme}-A100-l: calc(var(--mtc-l-A100) * 100%);
+    --${theme}-A100: hsl(var(--${theme}-A100-h), var(--${theme}-A100-s), var(--${theme}-A100-l));
     --${theme}-A100-contrast: hsl(0, 0%, calc((var(--mtc-l-A100) * 100% - var(--${theme}-contrast-threshold, 50%)) * (-100)));
-    --${theme}-A200: hsl(calc(var(--${theme}-h) * var(--mtc-h-A200)), calc(var(--mtc-s-A200) * 100%), calc(var(--mtc-l-A200) * 100%));
+
+    --${theme}-A200-h: calc(var(--${theme}-h) * var(--mtc-h-A200));
+    --${theme}-A200-s: calc(var(--mtc-s-A200) * 100%);
+    --${theme}-A200-l: calc(var(--mtc-l-A200) * 100%);
+    --${theme}-A200: hsl(var(--${theme}-A200-h), var(--${theme}-A200-s), var(--${theme}-A200-l));
     --${theme}-A200-contrast: hsl(0, 0%, calc((var(--mtc-l-A200) * 100% - var(--${theme}-contrast-threshold, 50%)) * (-100)));
-    --${theme}-A400: hsl(calc(var(--${theme}-h) * var(--mtc-h-A400)), calc(var(--mtc-s-A400) * 100%), calc(var(--mtc-l-A400) * 100%));
+
+    --${theme}-A400-h: calc(var(--${theme}-h) * var(--mtc-h-A400));
+    --${theme}-A400-s: calc(var(--mtc-s-A400) * 100%);
+    --${theme}-A400-l: calc(var(--mtc-l-A400) * 100%);
+    --${theme}-A400: hsl(var(--${theme}-A400-h), var(--${theme}-A400-s), var(--${theme}-A400-l));
     --${theme}-A400-contrast: hsl(0, 0%, calc((var(--mtc-l-A400) * 100% - var(--${theme}-contrast-threshold, 50%)) * (-100)));
-    --${theme}-A700: hsl(calc(var(--${theme}-h) * var(--mtc-h-A700)), calc(var(--mtc-s-A700) * 100%), calc(var(--mtc-l-A700) * 100%));
+
+    --${theme}-A700-h: calc(var(--${theme}-h) * var(--mtc-h-A700));
+    --${theme}-A700-s: calc(var(--mtc-s-A700) * 100%);
+    --${theme}-A700-l: calc(var(--mtc-l-A700) * 100%);
+    --${theme}-A700: hsl(var(--${theme}-A700-h), var(--${theme}-A700-s), var(--${theme}-A700-l));
     --${theme}-A700-contrast: hsl(0, 0%, calc((var(--mtc-l-A700) * 100% - var(--${theme}-contrast-threshold, 50%)) * (-100)));
-    
+
     
     /*
       Color weights
